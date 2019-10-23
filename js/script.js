ccf2e7deb52bf2d126a369afa61be0b1abadb03a
AakritiBagchi <aak28.ab@gmail.com>
2019-10-23 22:15:08 +0000

    java script put input into a string

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 $( "document" ).ready(function() { //this helps your jQuery to work. Write all of your JS code in this function.
-
+$( "#button" ).click(function() {
+    let word= 
+    $(".input").val();
+    console.log(word);
+});
 
 
   
